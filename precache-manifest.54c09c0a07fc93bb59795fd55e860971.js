fbcdb780249ecff55b88e3947f813bc1608bdcfd
Zotako <mkvarun2001@gmail.com>
2020-07-09 17:06:55 +0530

    Updates

---
+++ precache-manifest.54c09c0a07fc93bb59795fd55e860971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269e48ea052d8d5ac66411deed1b9abd",
+    "url": "/WorkIt/index.html"
+  },
+  {
+    "revision": "500f1dfe6823e2c4163a",
+    "url": "/WorkIt/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "0fd0888674880b911e99",
+    "url": "/WorkIt/static/css/main.9a1b3662.chunk.css"
+  },
+  {
+    "revision": "500f1dfe6823e2c4163a",
+    "url": "/WorkIt/static/js/2.9e718c4f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/WorkIt/static/js/2.9e718c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd0888674880b911e99",
+    "url": "/WorkIt/static/js/main.8d81c9e1.chunk.js"
+  },
+  {
+    "revision": "5817f583ad3b44702b15",
+    "url": "/WorkIt/static/js/runtime-main.c08a8157.js"
+  }
+]);
