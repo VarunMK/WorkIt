ea51bdcb02547d329ecac6474f749d1e7e16ada7
Zotako <mkvarun2001@gmail.com>
2020-07-12 22:08:32 +0530

    Updates

--- precache-manifest.c21810f27b81f7d46018cdb5800e9290.js
+++ precache-manifest.c21810f27b81f7d46018cdb5800e9290.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "710c36c76169c11fc28f4ea10b169d7b",
+    "revision": "d952cdac7874b2ab0ba8f480771afa71",
     "url": "/WorkIt/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/WorkIt/static/css/2.9edc93b8.chunk.css"
   },
   {
-    "revision": "e05c6391c42c71a46c09",
+    "revision": "d4729231bf2c1fc43e2f",
     "url": "/WorkIt/static/css/main.0980ec1d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/WorkIt/static/js/2.b52399cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e05c6391c42c71a46c09",
-    "url": "/WorkIt/static/js/main.fd54cd71.chunk.js"
+    "revision": "d4729231bf2c1fc43e2f",
+    "url": "/WorkIt/static/js/main.bed6fa72.chunk.js"
   },
   {
     "revision": "5817f583ad3b44702b15",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WorkIt/precache-manifest.ac85d568181a3519824621975251e6d7.js"
+  "/WorkIt/precache-manifest.c21810f27b81f7d46018cdb5800e9290.js"
 );
 
 self.addEventListener('message', (event) => {
